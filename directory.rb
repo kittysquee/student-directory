5cce922c6f37001f712cb76e76131a26a8939328
Elaine Osbourn <kittysquee@gmail.com>
2017-02-27 17:07:48 +0000

    Changes code to be the same as the end of exercise 7

--- directory.rb
+++ directory.rb
@@ -4,7 +4,7 @@ def print_header
 end
 
 def print(students)
-  students.each_with_index do |student|
+  students.each do |student|
     puts "#{student[:name]} (#{student[:cohort]} cohort)"
   end
 end
