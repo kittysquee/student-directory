ffaa842f9617ab4fd33302d8b165aadf9ab441dc
Elaine Osbourn <kittysquee@gmail.com>
2017-02-28 16:27:08 +0000

    Refactors code with exercise 10

--- directory9.rb
+++ directory9.rb
@@ -1,26 +1,49 @@
 #code from directory.rb
+@students = []
+
+def input_students
+  puts "Please enter the names of the students"
+  puts "To finish, just hit return twice"
+
+  name = gets.chomp
+
+  while !name.empty? do
+    @students << {name: name, cohort: :november}
+    puts "Now we have #{@students.count} students"
+    name = gets.chomp
+  end
+  # @students
+end
 
 def interactive_menu
-  students = []
   loop do
-    #1. Print menu and asj user what to do
-    puts "1. Input the students"
-    puts "2. Show the students"
-    puts "9. Exit" #no.9 as we'll be addign more items
+    print_menu
+    process(gets.chomp)
+  end
+end
 
-    selection = gets.chomp #2. read input and save to variable
-    case selection #3. do what user has asked
-    when "1"
-      students = input_students
-    when "2"
-      print_header
-      printer(students)
-      print_footer(students)
-    when "9"
-      exit #terminate program
-    else
-      puts "I don't know what you meant, try again."
-    end
+def print_menu
+  puts "1. Input the students"
+  puts "2. Show the students"
+  puts "9. Exit" #no.9 as we'll be addign more items
+end
+
+def show_students
+  print_header
+  print_students_list
+  print_footer
+end
+
+def process(selection)
+  case selection #does what user asks
+  when "1"
+    input_students
+  when "2"
+    show_students
+  when "9"
+    exit
+  else
+    puts "I don't know what you meant, try again."
   end
 end
 
@@ -29,31 +52,14 @@ def print_header
   puts "-------------"
 end
 
-def printer(students)
-  students.each do |student|
+def print_students_list
+  @students.each do |student|
     puts "#{student[:name]} (#{student[:cohort]} cohort)"
   end
 end
 
-def print_footer(names)
-  puts "Overall, we have #{names.count} great students"
-end
-
-
-def input_students
-  puts "Please enter the names of the students"
-  puts "To finish, just hit return twice"
-
-  students = []
-
-  name = gets.chomp
-
-  while !name.empty? do
-    students << {name: name, cohort: :november}
-    puts "Now we have #{students.count} students"
-    name = gets.chomp
-  end
-  students
+def print_footer
+  puts "Overall, we have #{@students.count} great students"
 end
 
 interactive_menu
