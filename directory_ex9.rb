a40e412a5ae24ea19a482f09f264a3c95e5ae8dc
Elaine Osbourn <kittysquee@gmail.com>
2017-02-28 12:39:50 +0000

    Adds correct pleuralisation to student count

---
+++ directory_ex9.rb
@@ -0,0 +1,42 @@
+#Right now if we have only one student, the user will see a message "Now we have 1 students", whereas it should be "Now we have 1 student". How can you fix it so that it used singular form when appropriate and plural form otherwise?
+
+def print_header
+  puts "The students of Villains Academy"
+  puts "-------------"
+end
+
+def printer(students)
+  students.each do |student|
+    puts "#{student[:name]} (#{student[:cohort]} cohort)"
+  end
+end
+
+def print_footer(names)
+  if names.count == 1
+    puts "Overall, we have #{names.count} great student"
+  else
+    puts "Overall, we have #{names.count} great students"
+  end
+end
+
+
+def input_students
+  puts "Please enter the names of the students"
+  puts "To finish, just hit return twice"
+
+  students = []
+
+  name = gets.chomp
+
+  while !name.empty? do
+    students << {name: name, cohort: :november}
+    puts "Now we have #{students.count} students"
+    name = gets.chomp
+  end
+  students
+end
+
+students = input_students
+print_header
+printer(students)
+print_footer(students)
